3dd2f594e02cc68612f82af7d658f78e5e5ed34f
Yun Lin <llmhyy@gmail.com>
2017-07-19 17:11:47 +0800

    add an example

---
+++ apache-common-math-2.2/src/test/java/com/Example.java
@@ -0,0 +1,18 @@
+package com;
+
+public class Example {
+	
+	int b;
+	
+	public void test(int a){
+		if(Math.pow(2, b) < 100){
+			if(a*a > 40){
+				System.currentTimeMillis();
+			}
+			
+			System.currentTimeMillis();
+		}
+		
+		System.currentTimeMillis();
+	}
+}
